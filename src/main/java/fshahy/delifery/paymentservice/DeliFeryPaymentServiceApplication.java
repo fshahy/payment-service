64a0119486c3c8568c2a66b602ceb4d0d07f0e96
Farid Shahy <fshahy@gmail.com>
2020-11-12 18:22:15 +0330

    init maven project.

---
+++ src/main/java/fshahy/delifery/paymentservice/DeliFeryPaymentServiceApplication.java
@@ -0,0 +1,13 @@
+package fshahy.delifery.paymentservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliFeryPaymentServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeliFeryPaymentServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fshahy/delifery/paymentservice/DeliFeryPaymentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package fshahy.delifery.paymentservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliFeryPaymentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
